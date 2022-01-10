f384f49a0c63601fbe4125f989777829644d503c
opilane <opilane@HP-CZC6297BBB>
2022-01-10 16:18:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Variant4EmVlad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - proverka na palindrom");
+            string valik = Console.ReadLine();
+            if (int.Parse(valik)==1)
+            {
+                ProverkaNaPalindrom.proverka();
+            }
+
+        }
+    }
+}
---
+++ ProverkaNaPalindrom.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Variant4EmVlad
+{
+    public class ProverkaNaPalindrom
+    {
+        public static void proverka()
+        {
+            Console.WriteLine("Vvedite proizvolniy tekst:  ");
+            string a = Console.ReadLine();
+            a = a.Replace(" ", "").ToLower();
+            Console.WriteLine(a);
+            var b = new string(a.Reverse().ToArray());
+            if (a == b)
+            {
+                Console.WriteLine("Eto palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("Eto ne palindrom!");
+            }
+            Console.ReadLine();
+        }
+        public static void matrica()
+        {
+            Console.WriteLine("Vvedite razmer kvadratnoi matricq:");
+            int r = int.Parse(Console.ReadLine());
+            int[,] matrica = new int[r, r];
+            Random randomiser = new Random();
+            int x;
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    x = randomiser.Next(-100, 100);
+                    matrica[i, j] = x;
+                }
+                for (int i = 0; i < r; i++)
+                {
+                    for (int j = 0; j < r; j++)
+                    {
+                        Console.Write(matrica[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+  
+
